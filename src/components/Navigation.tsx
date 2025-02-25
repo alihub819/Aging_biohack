0b0a94e68ba59fd1ab913946f1e749718be6cb90
alihub819 <153026146+alihub819@users.noreply.github.com>
2025-02-25 22:54:20 +0500

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navigation() {
   return (
     <>
-      <a href="/" className="text-gray-700 hover:text-teal-600">Home</a>
-      <a href="/treatments" className="text-gray-700 hover:text-teal-600">Treatments</a>
-      <a href="/about" className="text-gray-700 hover:text-teal-600">About</a>
-      <a href="/blog" className="text-gray-700 hover:text-teal-600">Blog</a>
-      <a href="/contact" className="text-gray-700 hover:text-teal-600">Contact</a>
-    </>
-  );
+      <Link to="/" className="text-gray-700 hover:text-teal-600">Home</Link>
+      <Link to="/treatments" className="text-gray-700 hover:text-teal-600">Treatments</Link>
+      <Link to="/about" className="text-gray-700 hover:text-teal-600">About</Link>
+      <Link to="/blog" className="text-gray-700 hover:text-teal-600">Blog</Link>
+      <Link to="/contact" className="text-gray-700 hover:text-teal-600">Contact</Link>
+    </>
+  );
 }
