2718cdb72e415455b726a4c99847ee1507fe73cf
alihub819 <aliamza819@gmail.com>
2025-02-25 23:30:08 +0500

    neoow

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,7 +11,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
             <div className="flex items-center">
-              <img src="./public\AgingBiohack.png" alt="" className='w-62 h-72 object-fill pb-2' />
+              <img src="https://res.cloudinary.com/dlasb4krd/image/upload/v1740508075/q8mvdcjiocqmtb1pmepp.png" alt="" className='w-62 h-72 object-fill pb-2' />
               {/* <a href="/" className="text-2xl font-bold text-teal-600">
                 Aging<span className="text-gray-800">Biohacks </span>
               </a> */}
